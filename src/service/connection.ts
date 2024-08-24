309e6ec8b3e3561d7e99c6bbed641cc6fcc7de2f
Leonardo-Mazzuca <leonardodesouzamazzuca8@gmail.com>
2024-08-24 14:09:05 -0300

    mudando url

--- src/service/connection.ts
+++ src/service/connection.ts
@@ -5,7 +5,7 @@ const createHttp = (): AxiosInstance => {
 
 
     const http = axios.create({
-        baseURL: 'https://85.31.61.50:3000',
+        baseURL: 'http://85.31.61.50:3000',
     });
     
 
